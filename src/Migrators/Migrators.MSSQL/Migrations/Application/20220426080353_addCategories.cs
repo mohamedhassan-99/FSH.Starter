7be56fd2156dc3d7916d2ef353ba968b982246ba
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-04-26 10:32:40 +0200

    AddCategoriesToDb

---
+++ src/Migrators/Migrators.MSSQL/Migrations/Application/20220426080353_addCategories.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MSSQL.Migrations.Application
+{
+    public partial class addCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                schema: "Catalog",
+                table: "Assets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_CategoryId",
+                schema: "Catalog",
+                table: "Assets",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_Categories_CategoryId",
+                schema: "Catalog",
+                table: "Assets",
+                column: "CategoryId",
+                principalSchema: "Catalog",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_Categories_CategoryId",
+                schema: "Catalog",
+                table: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "Catalog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assets_CategoryId",
+                schema: "Catalog",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                schema: "Catalog",
+                table: "Assets");
+        }
+    }
+}
