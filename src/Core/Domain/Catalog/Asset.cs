936b2c7eeec388af03b0aed9ab287e294c5fb4bb
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-04-27 11:31:57 +0200

    Adding Project Model To Asset

--- src/Core/Domain/Catalog/Asset.cs
+++ src/Core/Domain/Catalog/Asset.cs
@@ -8,10 +8,12 @@ public class Asset : AuditableEntity, IAggregateRoot
     public string? ImagePath { get; private set; }
     public Guid BrandId { get; private set; }
     public Guid CategoryId { get; private set; }
+    public Guid ProjectId { get; private set; }
     public virtual Brand Brand { get; private set; } = default!;
     public virtual Category Category { get; private set; } = default!;
+    public virtual Project Project { get; private set; } = default!;
 
-    public Asset(string name, string? description, decimal rate, Guid brandId, Guid categoryId, string? imagePath)
+    public Asset(string name, string? description, decimal rate, Guid brandId, Guid categoryId, Guid projectId, string? imagePath)
     {
         Name = name;
         Description = description;
@@ -19,15 +21,17 @@ public class Asset : AuditableEntity, IAggregateRoot
         ImagePath = imagePath;
         BrandId = brandId;
         CategoryId = categoryId;
+        ProjectId = projectId;
     }
 
-    public Asset Update(string? name, string? description, decimal? rate, Guid? brandId, Guid? categoryId, string? imagePath)
+    public Asset Update(string? name, string? description, decimal? rate, Guid? brandId, Guid? categoryId, Guid? projectId, string? imagePath)
     {
         if (name is not null && Name?.Equals(name) is not true) Name = name;
         if (description is not null && Description?.Equals(description) is not true) Description = description;
         if (rate.HasValue && Rate != rate) Rate = rate.Value;
         if (brandId.HasValue && brandId.Value != Guid.Empty && !BrandId.Equals(brandId.Value)) BrandId = brandId.Value;
         if (categoryId.HasValue && categoryId.Value != Guid.Empty && !CategoryId.Equals(categoryId.Value)) CategoryId = categoryId.Value;
+        if (projectId.HasValue && projectId.Value != Guid.Empty && !ProjectId.Equals(projectId.Value)) ProjectId = projectId.Value;
         if (imagePath is not null && ImagePath?.Equals(imagePath) is not true) ImagePath = imagePath;
         return this;
     }
