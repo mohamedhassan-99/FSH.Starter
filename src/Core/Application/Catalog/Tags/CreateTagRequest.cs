76766630dc45f0e1ff046e23e46f9142014dcf78
Ibrahim <Ibmsays.khidr@gmail.com>
2022-05-08 13:10:26 +0200

    create Tag

--- src/Core/Application/Catalog/Tags/CreateTagRequest.cs
+++ src/Core/Application/Catalog/Tags/CreateTagRequest.cs
@@ -3,16 +3,36 @@ public class CreateTagRequest : IRequest<Guid>
 {
     public string Name { get; set; } = default!;
     public string? Color { get; set; }
+    //public IList<Asset>? Assets { get; set; }
+
+
 }
 public class CreateTagRequestValidator : CustomValidator<CreateTagRequest>
 {
 
     public CreateTagRequestValidator(IReadRepository<Tag> repository, IStringLocalizer<CreateTagRequestValidator> localizer)
     {
-        RuleFor(p => p.Name)
-            .NotEmpty()
-            .MaximumLength(75)
-            .MustAsync(async (name, ct) => await repository.GetBySpecAsync(new TagByNameSpec(name), ct) is not null)
-            .WithMessage((_, name) => string.Format(localizer["tag.alreadyexists"], name));
+       // RuleFor(p => p.Name)
+       //     .NotEmpty()
+       //     .MaximumLength(75)
+       //     .MustAsync(async (name, ct) => await repository.GetBySpecAsync(new TagByNameSpec(name), ct) is not null)
+       //     .WithMessage((_, name) => string.Format(localizer["tag.alreadyexists"], name));
+    }
+}
+
+public class CreateTagRequestHandler : IRequestHandler<CreateTagRequest, Guid>
+{
+    // Add Domain Events automatically by using IRepositoryWithEvents
+    private readonly IRepositoryWithEvents<Tag> _repository;
+
+    public CreateTagRequestHandler(IRepositoryWithEvents<Tag> repository) => _repository = repository;
+
+    public async Task<Guid> Handle(CreateTagRequest request, CancellationToken cancellationToken)
+    {
+        var tag = new Tag(request.Name, request.Color,null );
+
+        await _repository.AddAsync(tag, cancellationToken);
+
+        return tag.Id;
     }
 }
